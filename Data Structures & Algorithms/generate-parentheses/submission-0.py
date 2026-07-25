ea6ef8df7823bcda0cebd1ffda4c8a8b6bf60578
keneolusanya <kenechukwu.olusanya@gmail.com>
2026-07-25 16:31:48 +0100

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        o, c = n, n
+        # arr for each path
+        # o, c represent options left
+        def build(o, c, par):
+            if o == 0 and c == 0:
+                res.append(par)
+                return
+            
+            if o <= c:
+                par1, par2 = par, par
+                if o > 0:
+                    par1 += "("
+                    build(o - 1, c, par1)
+                if c > 0:
+                    par2 += ")"
+                    build(o, c - 1, par2)
+        
+        build(n, n, "")
+        return res
