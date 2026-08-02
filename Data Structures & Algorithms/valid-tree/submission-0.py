42aa75100ccb340359a6f657a2d9ee7c3fb09dd1
keneolusanya <kenechukwu.olusanya@gmail.com>
2026-08-03 00:05:45 +0100

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = {i : [] for i in range(n)}
+        seen = set()
+
+        # back and forth involving three or mor enodes
+        # what if you basically ignored a most recently
+        # relevant node when processsing?
+       
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def dfs(node, par):
+            if node in seen and node != par:
+                print("first")
+                print(node)
+                print(par)
+                return False
+
+            seen.add(node)
+
+            for u in adj[node]:
+                if u != par:
+                    if not dfs(u, node):
+                        return False
+
+            return True
+        
+        return dfs(0, None) and len(seen) == n
